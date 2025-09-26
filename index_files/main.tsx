7d686d17bdfcfb0d3b806e6ba9ad4f4653c6b186
Sashen <sashen425@gmail.com>
2025-09-26 10:54:54 +0530

    Add files via upload

---
+++ index_files/main.tsx
@@ -0,0 +1,19 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=188b962d"; const _jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=188b962d"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import App from "/src/App.tsx";
+import "/src/index.css";
+createRoot(document.getElementById("root")).render(/*#__PURE__*/ _jsxDEV(App, {
+    "data-lov-id": "src\\main.tsx:5:52",
+    "data-lov-name": "App",
+    "data-component-path": "src\\main.tsx",
+    "data-component-line": "5",
+    "data-component-file": "main.tsx",
+    "data-component-name": "App",
+    "data-component-content": "%7B%7D"
+}, void 0, false, {
+    fileName: "D:/Projects - DEV/WEB/B-Day v1/src/main.tsx",
+    lineNumber: 5,
+    columnNumber: 53
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IjtBQUFBLFNBQVNBLFVBQVUsUUFBUSxtQkFBa0I7QUFDN0MsT0FBT0MsU0FBUyxZQUFXO0FBQzNCLE9BQU8sY0FBYTtBQUVwQkQsV0FBV0UsU0FBU0MsY0FBYyxDQUFDLFNBQVVDLE1BQU0sZUFBQyxRQUFDSDtJQUFFSSxlQUFBO0lBQUFDLGlCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLHVCQUFBO0lBQUFDLDBCQUFBIiwibmFtZXMiOlsiY3JlYXRlUm9vdCIsIkFwcCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiLCJkYXRhLWxvdi1pZCIsImRhdGEtbG92LW5hbWUiLCJkYXRhLWNvbXBvbmVudC1wYXRoIiwiZGF0YS1jb21wb25lbnQtbGluZSIsImRhdGEtY29tcG9uZW50LWZpbGUiLCJkYXRhLWNvbXBvbmVudC1uYW1lIiwiZGF0YS1jb21wb25lbnQtY29udGVudCJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSBcInJlYWN0LWRvbS9jbGllbnRcIjtcbmltcG9ydCBBcHAgZnJvbSBcIi4vQXBwLnRzeFwiO1xuaW1wb3J0IFwiLi9pbmRleC5jc3NcIjtcblxuY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikhKS5yZW5kZXIoPEFwcCAvPik7XG4iXSwiZmlsZSI6IkQ6L1Byb2plY3RzIC0gREVWL1dFQi9CLURheSB2MS9zcmMvbWFpbi50c3gifQ==
